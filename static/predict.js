621c5db2ce0b4dfd7679733ae665d845ddaa820e
Sascha Dittmann <Sascha5812@gmx.de>
2020-08-04 18:48:50 +0200

    refactoring predict function

--- static/predict.js
+++ static/predict.js
@@ -11,10 +11,12 @@ $("#image-selector").change(function () {
 });
 
 let model;
+let is_new_od_model;
 $( document ).ready(async function () {
 	$('.progress-bar').show();
     console.log( "Loading model..." );
 	model = await tf.loadGraphModel('model/model.json');
+	is_new_od_model = model.inputs.length == 3;
 	console.log( "Model loaded." );
 	$('.progress-bar').hide();
 });
@@ -28,15 +30,12 @@ function _logistic(x) {
 	}
 }
 
-const ANCHORS = [0.573, 0.677, 1.87, 2.06, 3.34, 5.47, 7.88, 3.53, 9.77, 9.17];
-const NEW_OD_OUTPUT_TENSORS = ['detected_boxes', 'detected_scores', 'detected_classes'];
-$("#predict-button").click(async function () {
+function loadImage() {
 	const pixels = $('#selected-image').get(0);
 	
 	// Pre-process the image
 	console.log( "Loading image..." );
 	const input_size = model.inputs[0].shape[1];
-	const is_new_od_model = model.inputs.length == 3;
 	let image = tf.browser.fromPixels(pixels, 3);
 	image = tf.image.resizeBilinear(image.expandDims().toFloat(), [input_size, input_size]);
 	if (is_new_od_model) {
@@ -44,8 +43,14 @@ $("#predict-button").click(async function () {
 		image = is_new_od_model ? image : image.reverse(-1); // RGB->BGR for old models
 	}
 
+	return image;
+}
+
+const ANCHORS = [0.573, 0.677, 1.87, 2.06, 3.34, 5.47, 7.88, 3.53, 9.77, 9.17];
+const NEW_OD_OUTPUT_TENSORS = ['detected_boxes', 'detected_scores', 'detected_classes'];
+async function predictLogos(inputs) {
 	console.log( "Running predictions..." );
-	const outputs = await model.executeAsync(image, is_new_od_model ? NEW_OD_OUTPUT_TENSORS : null);
+	const outputs = await model.executeAsync(inputs, is_new_od_model ? NEW_OD_OUTPUT_TENSORS : null);
 	const arrays = !Array.isArray(outputs) ? outputs.array() : Promise.all(outputs.map(t => t.array()));
 	let predictions = await arrays;
 
@@ -93,6 +98,55 @@ $("#predict-button").click(async function () {
 		const selected_indices = await tf.image.nonMaxSuppressionAsync(boxes, scores, 10);
 		predictions = [await boxes.gather(selected_indices).array(), await scores.gather(selected_indices).array(), await classes.gather(selected_indices).array()];
 	}
+
+	return predictions;
+}
+
+var children = [];
+function drawResults() {
+  // Now let's start classifying a frame in the stream.
+  model.detect(video).then(function (predictions) {
+    // Remove any highlighting we did previous frame.
+    for (let i = 0; i < children.length; i++) {
+      liveView.removeChild(children[i]);
+    }
+    children.splice(0);
+    
+    // Now lets loop through predictions and draw them to the live view if
+    // they have a high confidence score.
+    for (let n = 0; n < predictions.length; n++) {
+      // If we are over 66% sure we are sure we classified it right, draw it!
+      if (predictions[n].score > 0.66) {
+        const p = document.createElement('p');
+        p.innerText = predictions[n].class  + ' - with ' 
+            + Math.round(parseFloat(predictions[n].score) * 100) 
+            + '% confidence.';
+        p.style = 'margin-left: ' + predictions[n].bbox[0] + 'px; margin-top: '
+            + (predictions[n].bbox[1] - 10) + 'px; width: ' 
+            + (predictions[n].bbox[2] - 10) + 'px; top: 0; left: 0;';
+
+        const highlighter = document.createElement('div');
+        highlighter.setAttribute('class', 'highlighter');
+        highlighter.style = 'left: ' + predictions[n].bbox[0] + 'px; top: '
+            + predictions[n].bbox[1] + 'px; width: ' 
+            + predictions[n].bbox[2] + 'px; height: '
+            + predictions[n].bbox[3] + 'px;';
+
+        liveView.appendChild(highlighter);
+        liveView.appendChild(p);
+        children.push(highlighter);
+        children.push(p);
+      }
+    }
+    
+    // Call this function again to keep predicting when the browser is ready.
+    window.requestAnimationFrame(predictWebcam);
+  });
+}
+
+$("#predict-button").click(async function () {
+	const image = loadImage();
+	const predictions = await predictLogos(image);
 
 	// TODO: Draw Bounding Boxes
 	console.log(predictions);
